d6ea653ef5f3b8606467f43c1fe474eb791e72e0
Hard12-j <joshi12hardik@gmail.com>
2025-09-18 21:31:31 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <div className="site">
+      <Header />
+      <main>
+        <Hero />
+        <Features />
+      </main>
+      <Footer />
+    </div>
+  );
+}
